4dbe56c57810515a56b7f2a21d119c84ef0bdaaf
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-18 19:16:46 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Week5FınalPractice
+{
+    public class Car
+    {
+        public DateTime productionDate;
+        public int serialNumber { get; set; }
+        public string? brand { get; set; }
+        public string? model { get; set; }
+        public string? color { get; set; }
+        public int doorCount { get; set; }
+
+        //Üretim Tarihi değeri araba üretilirken otomatik olarak o an olarak atanacak.
+        public Car ()
+        {
+           productionDate = DateTime.Now;
+        }
+
+       public void showCarDetails()
+        {
+            Console.WriteLine("ARAÇ DETAYLARI");
+
+            Console.WriteLine($"Aracın üretildiği tarih: {productionDate}");
+            Console.WriteLine($"Seri numarası: {serialNumber}");
+            Console.WriteLine($"Markası: {brand}");
+            Console.WriteLine($"Modeli: {model}");
+            Console.WriteLine($"Rengi: {color}");
+            Console.WriteLine($"Kapı sayısı: {doorCount}");
+            Console.WriteLine("");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using Week5FınalPractice;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        List<Car> carList = new List<Car>();
+
+        while (true)
+        {
+            Console.Write("Araba üretmek istiyor musunuz? Evet ise 'e' Hayır ise 'h' yazınız : ");
+            string userDecision = Console.ReadLine();
+
+            //Kullanıcının cevabı hayır programı sonlandıralım,
+            //evet ise bir araba nesnesi üretip özelliklerini konsol ekranından kullanıcıya girdirelim
+            if (userDecision.ToLower() == "h")
+            {
+                Console.WriteLine("Görüşmek üzere");
+                break;
+            }
+            if (userDecision.ToLower() == "e")
+            {
+                secondOrMoreCar:
+               Car newCar = new Car();
+                Console.Write("Aracınızın Seri numarasınız giriniz:");
+                newCar.serialNumber =Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Aracınızın Markasını giriniz:");
+                newCar.brand = Console.ReadLine();
+
+                Console.Write("Aracınızın modelini giriniz: ");
+                newCar.model = Console.ReadLine();
+
+                Console.Write("Aracınızın rengini giriniz: ");
+                newCar.color = Console.ReadLine();
+
+            //goto komutunu araştırınız.
+            SelectDoors:
+                Console.Write("Aracınızın kapı sayısını giriniz: ");
+                if (!int.TryParse(Console.ReadLine(), out int doors) || (doors != 2 && doors != 4))
+                {
+                    Console.WriteLine("sadece 2 veya 4 kapılı seçebilirsiniz");
+                    goto SelectDoors;
+                }
+                newCar.doorCount = doors;
+
+                carList.Add(newCar);
+
+                Console.Write("Başka bir araba daha üretmek ister misiniz? (evet veya hayır)");
+                string userMoreCarDecision = Console.ReadLine().ToLower();
+                if(userMoreCarDecision == "evet")
+                {
+                    goto secondOrMoreCar;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Yanlış bir cevap girdiniz lütfen tekrar deneyiniz!");
+            }
+
+        }
+
+        Console.WriteLine("Listedeki bütün araçlar:\n");
+        foreach (Car c in carList)
+        {
+            c.showCarDetails();
+        }
+
+    }
+
+}
+
+
